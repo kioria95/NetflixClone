a6106a02433d46355d3635c25c3c8caf94075d79
kioria95 <kenkioria@gmail.com>
2020-09-03 17:45:28 +0300

    Added Banner.css

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -18,6 +19,11 @@ function Banner() {
     fetchData();
   }, []);
 
+  function truncate(str,n)
+  {
+      return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <div>
       <header
@@ -30,19 +36,19 @@ function Banner() {
       >
         <div className="banner__contents">
           {/* title */}
-          <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+          <h1 className="banner__title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+
+          {/*div > 2 buttons*/}
+          <div className="banner__buttons">
+            <button className="banner__button">Play</button>
+            <button className="banner__button">My List</button>
+          </div>
+
+          {/*description*/}
+          <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
         </div>
-
-        {/*div > 2 buttons*/}
-        <div className="banner__buttons">
-          <button className="banner__button">
-
-          </button>
-          <button className="banner__button">
-
-          </button>
-        </div>
-        {/*description*/}
       </header>
     </div>
   );
